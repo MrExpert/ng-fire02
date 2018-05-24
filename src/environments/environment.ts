cfc987ddb228e64226141a81210c4bd153c66801
Sam Umirdinov <sam.umirdinov@gmail.com>
2018-05-24 11:58:30 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,14 @@
 
 export const environment = {
   production: false,
-  
+  firebaseConfig: {
+    apiKey: "AIzaSyDJuZ2n4JyYDxBmrdKIUKaHZ23xAL-xy2s",
+    authDomain: "fire-1f447.firebaseapp.com",
+    databaseURL: "https://fire-1f447.firebaseio.com",
+    projectId: "fire-1f447",
+    storageBucket: "fire-1f447.appspot.com",
+    messagingSenderId: "356110976200"
+  }
 };
 
 /*
